64883f2d9ffb0e19c554ce5349901dfd6c4577ae
Zenicide <zencapton@gmail.com>
2019-10-12 19:08:54 -0400

    Making time(not done)

---
+++ Assets/Scripts/timerHandle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerHandle : MonoBehaviour
+{
+
+    public double time = 90;
+    public int displayTime = 90;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (time >= 0) {
+            time -= 0.01;
+        }
+
+        if (time % 1 == 0)
+        {
+            displayTime = (int)time;
+        }
+    }
+}
