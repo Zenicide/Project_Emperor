d539781c4c03a9b1e1a6ab0998863a6c9cee6085
Jackson Robbins (RIT Student) <jsr5501@ad.rit.edu>
2019-10-12 16:18:17 -0400

    Put Ares on player One

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class ObjectManager : MonoBehaviour
 {
+    public GameObject playerOnePrefab;
+    public GameObject playerTwoPrefab;
+
+    GameObject playerOne;
+    GameObject playerTwo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerOne = Instantiate(playerOnePrefab);
+        playerOne.GetComponent<Character>().player = "one";
+        //playerTwo = Instantiate(playerTwoPrefab);
+
     }
 
     // Update is called once per frame
