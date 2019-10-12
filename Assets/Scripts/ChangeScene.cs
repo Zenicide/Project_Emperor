88c6ea942d2d7ddd629c018a20fc7ec494cf38fc
Zenicide <zencapton@gmail.com>
2019-10-12 15:25:12 -0400

    Added a scene manager to each scene
    Some may not need a scenemanager but we can take it out, it was mainy for testing purposes for a basic manager

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Windows.Input;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    const int totalScenes = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        if (Input.GetKeyDown("c"))
+        {
+            
+            SceneManager.LoadScene(index + 1);
+            index++;
+        }
+        if (index != 0)
+        {
+            if (Input.GetKeyDown("escape"))
+            {
+                SceneManager.LoadScene(4);
+            }
+        }
+        
+
+        if (index == 0) {
+
+            
+
+            if (Input.GetKeyDown("escape"))
+            {
+                Application.Quit();
+            }
+        }
+
+        if (index == 4)
+        {
+            if (Input.GetKeyDown("escape"))
+            {
+                SceneManager.LoadScene(3);
+            }
+        }
+    }
+}
