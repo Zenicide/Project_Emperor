f63b18d8dd06af40962d7d36414c55f688788e6c
Zenicide <zencapton@gmail.com>
2019-10-12 16:36:57 -0400

    Change to scene manager

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,23 +5,19 @@ using System.Windows.Input;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
+
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         int index = SceneManager.GetActiveScene().buildIndex;
-        if (Input.GetKeyDown("c"))
-        {
-            
-            SceneManager.LoadScene(index + 1);
-            index++;
-        }
+        
         
         // Testing uses of escape
         if (Input.GetKeyDown("escape"))
@@ -29,14 +25,30 @@ public class ChangeScene : MonoBehaviour
 
             switch (index)
             {
+
+                // If in the title screen, quit
                 case 0:
                     Application.Quit();
                     break;
-                case 3:
+
+                // If in play scene,
+
+                // Play screen to pause screen
+                case 1:
                     SceneManager.LoadScene(4);
                     break;
+                    // Practice screen to title screen
+                case 2:
+                    SceneManager.LoadScene(0);
+                    break;
+                    // Instructions to title screen
+                case 3:
+                    SceneManager.LoadScene(0);
+                    break;
+
+                    // Pause to play screen
                 case 4:
-                    SceneManager.LoadScene(3);
+                    SceneManager.LoadScene(1);
                     break;
 
             }
