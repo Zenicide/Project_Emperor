1411a2fd61d39f8454535754580609efcfe95be7
WhalesAreNice <an1994xp@gmail.com>
2019-10-13 09:55:28 -0400

    added the gameplayUI back in

---
+++ Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    GameObject Player1;
+    GameObject Player2;
+
+    CollisionDetection player1Detect;
+    CollisionDetection player2Detect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1Detect = gameObject.GetComponent<CollisionDetection>();
+        player2Detect = gameObject.GetComponent<CollisionDetection>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
